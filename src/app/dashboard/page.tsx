1b0887704651943bc49734c8571dabe27bc5cb47
Shankur Malik <sankurmalik222@gmail.com>
2026-09-21 20:29:18 +0530

    Fix dashboard dynamic rendering

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import ActivityOverview from "@/components/dashboard/ActivityOverview";
 import MainActions from "@/components/dashboard/MainActions";
 import WelcomeSection from "@/components/dashboard/WelcomeSection";

90fae6ebc87c06f23535452b1fe2a5ac7df3f05b
Shankur Malik <sankurmalik222@gmail.com>
2026-09-21 19:19:43 +0530

    Initial DentWise setup

---
+++ src/app/voice/page.tsx
@@ -0,0 +1,24 @@
+import Navbar from "@/components/Navbar";
+import FeatureCards from "@/components/voice/FeatureCards";
+import VapiWidget from "@/components/voice/VapiWidget";
+import WelcomeSection from "@/components/voice/WelcomeSection";
+import { auth } from "@clerk/nextjs/server";
+
+async function VoicePage() {
+  await auth();
+
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <FeatureCards />
+      </div>
+
+      <VapiWidget />
+    </div>
+  );
+}
+
+export default VoicePage;
